d530fde4a526a87224aea8c5bc7044be76fe680f
Hannah Dugdale <h.l.dugdale@rug.nl>
2021-01-18 09:13:37 +0100

    changed clean.sh location

---
+++ WEEK2_DAY3_EXERCISE/GITHUB_TEST/clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#GitHub_Test clean
+#Purpose: remove DATD and RESULTS sub-directories
+#Written by: Hannah Dugdale
+#Date: 12 January 2021
+
+rm -r 1_ANNOTATION/DATA GITHUB_TEST/1_ANNOTATION/RESULTS 
+rm -r 2_ALPHABET/DATA GITHUB_TEST/2_ALPHABET/RESULTS 
+rm -r 3_NUMBERS/DATA GITHUB_TEST/3_NUMBERS/RESULTS 
--- WEEK2_DAY3_EXERCISE/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#GitHub_Test clean
-#Purpose: remove data and results sub-directories
-#Written by: Hannah Dugdale
-#Date: 12 January 2021
-
-rm -r GITHUB_TEST/1_ANNOTATION/DATA GITHUB_TEST/1_ANNOTATION/RESULTS 
-rm -r GITHUB_TEST/2_ALPHABET/DATA GITHUB_TEST/2_ALPHABET/RESULTS 
-rm -r GITHUB_TEST/3_NUMBERS/DATA GITHUB_TEST/3_NUMBERS/RESULTS 
